f19f0207218c123fa4aed3cfaed66b8e8facd9a2
Lukas Schmyrczyk <lukas.schmyrczyk@lionmint.com>
2023-03-22 22:24:19 +0100

    Format

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -2,19 +2,19 @@
 
 -- Only required if you have packer configured as `opt`
 vim.cmd([[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerCompile
-  augroup end
+augroup packer_user_config
+autocmd!
+autocmd BufWritePost plugins.lua source <afile> | PackerCompile
+augroup end
 ]])
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+-- Packer can manage itself
+use 'wbthomason/packer.nvim'
 
-  use 'williamboman/mason.nvim'    
-  use 'williamboman/mason-lspconfig.nvim'
-  use 'neovim/nvim-lspconfig' 
+use 'williamboman/mason.nvim'    
+use 'williamboman/mason-lspconfig.nvim'
+use 'neovim/nvim-lspconfig' 
 
 use 'simrat39/rust-tools.nvim'
 
@@ -25,37 +25,37 @@ use 'mfussenegger/nvim-dap'
 
 use 'nvim-treesitter/nvim-treesitter'
 
-    use 'puremourning/vimspector'
-  
-    use 'voldikss/vim-floaterm'
+use 'puremourning/vimspector'
 
-    -- Completion framework:
-    use 'hrsh7th/nvim-cmp' 
+use 'voldikss/vim-floaterm'
 
-    -- LSP completion source:
-    use 'hrsh7th/cmp-nvim-lsp'
+-- Completion framework:
+use 'hrsh7th/nvim-cmp' 
 
-    -- Useful completion sources:
-    use 'hrsh7th/cmp-nvim-lua'
-    use 'hrsh7th/cmp-nvim-lsp-signature-help'
-    use 'hrsh7th/cmp-vsnip'                             
-    use 'hrsh7th/cmp-path'                              
-    use 'hrsh7th/cmp-buffer'                            
-    use 'hrsh7th/vim-vsnip'
+-- LSP completion source:
+use 'hrsh7th/cmp-nvim-lsp'
+
+-- Useful completion sources:
+use 'hrsh7th/cmp-nvim-lua'
+use 'hrsh7th/cmp-nvim-lsp-signature-help'
+use 'hrsh7th/cmp-vsnip'                             
+use 'hrsh7th/cmp-path'                              
+use 'hrsh7th/cmp-buffer'                            
+use 'hrsh7th/vim-vsnip'
 
 use {
-  'nvim-telescope/telescope.nvim', tag = '0.1.1',
+'nvim-telescope/telescope.nvim', tag = '0.1.1',
 -- or                            , branch = '0.1.x',
-  requires = { {'nvim-lua/plenary.nvim'} }
+requires = { {'nvim-lua/plenary.nvim'} }
 }
 
 use({ 'rose-pine/neovim', as = 'rose-pine' })
 
 use {
-  'nvim-tree/nvim-tree.lua',
-  requires = {
-    'nvim-tree/nvim-web-devicons', -- optional
-  },
+'nvim-tree/nvim-tree.lua',
+requires = {
+'nvim-tree/nvim-web-devicons', -- optional
+},
 }
 
 end)
