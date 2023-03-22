8e5e0af1fcad509a62a6e2e255f55f57628727cb
Lukas Schmyrczyk <lukas.schmyrczyk@lionmint.com>
2023-03-22 22:19:09 +0100

    Initial commit

---
+++ lua/keys.lua
@@ -0,0 +1,23 @@
+-- telescope
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<C-ff', builtin.git_files, {})
+vim.keymap.set('n', '<leader>fs', function()
+                builtin.grep_string({search = vim.fn.input("Grep > ")});
+        end)
+
+-- Space PV back 
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+
+-- Vimspector
+vim.cmd([[
+nmap <F9> <cmd>call vimspector#Launch()<cr>
+nmap <F5> <cmd>call vimspector#StepOver()<cr>
+nmap <F8> <cmd>call vimspector#Reset()<cr>
+nmap <F11> <cmd>call vimspector#StepOver()<cr>")
+nmap <F12> <cmd>call vimspector#StepOut()<cr>")
+nmap <F10> <cmd>call vimspector#StepInto()<cr>")
+]])
+
