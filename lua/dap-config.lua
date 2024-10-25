1cdc90e1c88bad278a26fa03941f07ace66505ca
sascha10000 <github@duepre-sa.de>
2024-10-26 01:46:56 +0200

    added dap for rust WIP

--- lua/dap-config.lua
+++ lua/dap-config.lua
@@ -69,28 +69,58 @@ dap.adapters['node-js'] = {
     args = { os.getenv('HOME') .. '/dev/js-debug/src/dapDebugServer.js' },
 }
 
-dap.adapters.codelldb = {
-  type = 'server',
-  port = "${port}",
-  executable = {
-    command = '/usr/bin/codelldb',
-    args = {"--port", "${port}"},
-  }
+dap.adapters.lldb = {
+    type = 'executable',
+    command = '/usr/bin/lldb', -- adjust as needed, must be absolute path
+    name = 'lldb'
 }
 
 dap.configurations.rust = {
     {
-        name = "Rust debug",
-        type = "codelldb",
-        request = "launch",
+        name = 'Launch',
+        type = 'lldb',
+        request = 'launch',
         program = function()
-            return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/target/debug/', 'file')
+            return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/target', 'file')
         end,
         cwd = '${workspaceFolder}',
-        stopOnEntry = true,
+        stopOnEntry = false,
+        args = {},
+        initCommands = function()
+            -- Find out where to look for the pretty printer Python module
+            local rustc_sysroot = vim.fn.trim(vim.fn.system('rustc --print sysroot'))
+
+            local script_import = 'command script import "' .. rustc_sysroot .. '/lib/rustlib/etc/lldb_lookup.py"'
+            local commands_file = rustc_sysroot .. '/lib/rustlib/etc/lldb_commands'
+
+            local commands = {}
+            local file = io.open(commands_file, 'r')
+            if file then
+                for line in file:lines() do
+                    table.insert(commands, line)
+                end
+                file:close()
+            end
+            table.insert(commands, 1, script_import)
+
+            return commands
+        end,
+
+
+        -- 💀
+        -- if you change `runInTerminal` to true, you might need to change the yama/ptrace_scope setting:
+        --
+        --    echo 0 | sudo tee /proc/sys/kernel/yama/ptrace_scope
+        --
+        -- Otherwise you might get the following error:
+        --
+        --    Error on launch: Failed to attach to the target process
+        --
+        -- But you should be aware of the implications:
+        -- https://www.kernel.org/doc/html/latest/admin-guide/LSM/Yama.html
+        -- runInTerminal = false,
     },
 }
-
 dap.configurations.go = {
     {
         type = 'go',
